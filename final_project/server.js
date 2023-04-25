bc7f0701d16c45fc0f861f52501699c6ddbdcc67
John Spangle <jspangle2@ivytech.edu>
2023-04-24 21:53:24 -0400

    WIP-need to do additional research later on regarding CSS & JS files.-JS

--- final_project/server.js
+++ final_project/server.js
@@ -3,3 +3,48 @@
 // Required modules for the server
 const http = require("http");
 const fs = require("fs");
+
+// Server creation
+const server = http.createServer((req, res) => {
+  // monitor what is going on with each request
+  console.log(req.url, req.method);
+
+  // Page handling
+  let path = "./views/";
+  switch (req.url) {
+    case '/':
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case '/login':
+      path += "login.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += 'error404.html'
+      res.statusCode = 404;
+      break;
+  }
+
+  // Send an HTML file
+  fs.readFile (path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  })
+});
+
+// Listen for port 3000 on localhost
+// Port 3000 will be used for testing
+// Can be changed later if necessary
+server.listen(3000, 'localhost', () => {
+  // listening message
+  console.log("Listening for requests on Port 3000");
+});
