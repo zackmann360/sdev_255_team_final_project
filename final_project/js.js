2dc9cc56dd16585369d9a7990ffd750fcbd530c2
JohnSpangleIVY <jspangle2@ivytech.edu>
2023-04-24 20:21:21 -0400

    First round of tweaks and new pages. Nore to come shortly. -JS

--- final_project/js.js
+++ final_project/js.js
@@ -1,10 +1,11 @@
 var app = document.querySelector('.app')
 var prevScrollPos = window.pageYOffset
+
 window.onscroll = function(){
     var currentScrollPos = window.pageYOffset
-    if(prevScrollPos > currentScrollPos) {
+    if (prevScrollPos > currentScrollPos) {
         app.style.top = "0"
-    }else {
+    } else {
         app.style.top = "-10rem"
     }
     prevScrollPos = currentScrollPos
